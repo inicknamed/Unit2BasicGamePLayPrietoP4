cd61042d3b7ac57a1aeba897dc4e72365643f6c6
Damien <756225@student.dusd.net>
2026-03-09 11:33:15 -0700

    Ajusted Pizza shooter

---
+++ My project/Assets/Scripts/DesstroyOutOfBounds.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DesstroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 50.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
